1d846042817ff9f9002176bd5cbd23b268586279
Guilherme Martins <guimsl2004@gmail.com>
2025-05-28 20:05:12 -0300

    Ajuste nos icon vetores e criação de component vetor

--- portplace/src/app/pages/dashboard/dashboard.component.ts
+++ portplace/src/app/pages/dashboard/dashboard.component.ts
@@ -4,13 +4,14 @@ import { CardComponent } from '../../components/card/card.component';
 import { FormsModule } from '@angular/forms';
 import { BadgeComponent } from '../../components/badge/badge.component';
 import { ProgressComponent } from '../../components/progress/progress.component';
-import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { SvgIconComponent } from '../../components/svg-icon/svg-icon.component';
 
 interface MetricCard {
   title: string;
   subtitle: string;
   color: string;
   value?: string;
+  icon: string;
 }
 
 interface Risk {
@@ -33,7 +34,7 @@ interface Risk {
     FormsModule,
     ProgressComponent,
     BadgeComponent,
-    SidebarComponent
+    SvgIconComponent
   ],
 })
 export class DashboardComponent implements OnInit {
@@ -42,11 +43,11 @@ export class DashboardComponent implements OnInit {
   selectedPortfolio = 'portfolio1';
 
   metricCards: MetricCard[] = [
-    { title: 'Dentro do prazo', subtitle: 'Status do projeto', color: 'green' },
-    { title: 'Ativo do planejado', subtitle: 'Status', color: 'blue' },
-    { title: 'Estratégia 1', subtitle: 'Estratégia ativa', color: 'purple' },
-    { title: 'R$ 1.000.000,00', subtitle: 'Valor total', color: 'none', value: 'R$ 1.000.000,00' },
-    { title: 'Gabriel Martins', subtitle: 'Responsável', color: 'none' }
+    { title: 'Dentro do prazo', subtitle: 'Status do projeto', color: 'green', icon:'assets/icon/calendario_vetor.svg' },
+    { title: 'Ativo do planejado', subtitle: 'Status', color: 'blue', icon:'assets/icon/paid_vetor.svg' },
+    { title: 'Estratégia 1', subtitle: 'Estratégia ativa', color: 'purple', icon:'assets/icon/estrategia_vetor.svg' },
+    { title: 'R$ 1.000.000,00', subtitle: 'Valor total', color: 'none', value: 'R$ 1.000.000,00', icon:'assets/icon/porco_vetor.svg' },
+    { title: 'Gabriel Martins', subtitle: 'Responsável', color: 'none', icon:'assets/icon/user_vetor.svg  ' }
   ];
 
   risks: Risk[] = [
--- portplace/src/app/pages/home/home.component.ts
+++ portplace/src/app/pages/home/home.component.ts
@@ -12,12 +12,13 @@ import {
 } from 'lucide-angular';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { SvgIconComponent } from '../../components/svg-icon/svg-icon.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,SidebarComponent],
+  imports: [CommonModule,SvgIconComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -29,7 +30,7 @@ export class HomeComponent {
       id: 'dashboard',
       title: 'Dashboard',
       description: 'Monitore portfólios em andamento com controle de práticos',
-      icon: 'assets/icon/dashboard_icon.svg',
+      icon: 'assets/icon/dashboard_vetor.svg',
       route: '/dashboard',
       color: '#3b82f6'
     },
@@ -37,7 +38,7 @@ export class HomeComponent {
       id: 'portfolios',
       title: 'Portfólios',
       description: 'Gerencie e cadastre seus portfólios, seus riscos, comunicações e responsáveis',
-      icon: 'assets/icon/hub_portfolios_icon.svg',
+      icon: 'assets/icon/hub_portfolios_vetor.svg',
       route: '/portfolios',
       color: '#8b5cf6'
     },
@@ -45,7 +46,7 @@ export class HomeComponent {
       id: 'projects',
       title: 'Projetos',
       description: 'Gerencie e cadastre seus projetos',
-      icon: 'assets/icon/assignment_projetos_icon.svg',
+      icon: 'assets/icon/assignment_projetos_vetor.svg',
       route: '/projects',
       color: '#10b981'
     },
@@ -53,7 +54,7 @@ export class HomeComponent {
       id: 'strategies',
       title: 'Estratégias',
       description: 'Gerencie e cadastre as estratégias para avaliar projetos e os vínculos a portfólios',
-      icon: 'assets/icon/estrategia_icon.svg',
+      icon: 'assets/icon/estrategia_vetor.svg',
       route: '/strategies',
       color: '#f59e0b'
     },
@@ -61,7 +62,7 @@ export class HomeComponent {
       id: 'resources',
       title: 'Recursos',
       description: 'Gerencie cargos, pessoas e alocações de recursos humanos',
-      icon: 'assets/icon/recursos_icon.svg',
+      icon: 'assets/icon/recursos_vetor.svg',
       route: '/resources',
       color: '#ef4444'
     },
@@ -69,7 +70,7 @@ export class HomeComponent {
       id: 'users',
       title: 'Usuários',
       description: 'Gerencie o cadastro dos usuários do sistema',
-      icon: 'assets/icon/usuarios_icon.svg',
+      icon: 'assets/icon/usuarios_vetor.svg',
       route: '/users',
       color: '#6b7280'
     }
