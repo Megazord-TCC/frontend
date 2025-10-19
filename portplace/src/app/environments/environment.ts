b731ab97de040c9ac659c35f83c7640cd1e0bbac
Guilherme Martins <guimsl2004@gmail.com>
2025-10-19 11:18:32 -0300

    mudando env para back na núvem

--- portplace/src/app/environments/environment.ts
+++ portplace/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://tcc-portplace-back.kindtree-92b15a79.brazilsouth.azurecontainerapps.io/api/v1',
+  apiUrl: 'https://portplace-backend.onrender.com/api/v1',
   //apiUrl: 'http://localhost:8080/api/v1',
 };
