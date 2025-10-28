0f10a23b6723f91ff245ec04c059e76d3a427af6
Guilherme Martins <guimsl2004@gmail.com>
2025-10-27 22:57:25 -0300

    env

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://portplace-backend.onrender.com/api/v1',
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'https://portplace-backend.onrender.com/api/v1',
+  // apiUrl: 'http://localhost:8080/api/v1',
 };
